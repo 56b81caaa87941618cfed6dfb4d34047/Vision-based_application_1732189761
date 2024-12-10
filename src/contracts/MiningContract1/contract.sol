d948a981197746390bb4a324101f7b15d53b4101
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-10 12:43:29 +0100

    feat: Generate smart contract MiningContract1
    Requested contract: A smart contract for mining rewards on the HOLESKY network.

--- src/contracts/MiningContract1/contract.sol
+++ src/contracts/MiningContract1/contract.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
@@ -8,23 +7,24 @@ import { DataTypes } from "@ZKPay/types/DataTypes.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract AirdropClient is IZKPayClient {
+contract AirdropClient {
     using SafeERC20 for IERC20;
 
-    event LogError(uint8 errorCode, string errorMessage);
-
     address public _owner;
-    address public _zkpay;
-    IERC20 public immutable _token;
-    uint256 public constant AIRDROP_AMOUNT = 150 * 10 ** 18; // 150 tokens with 18 decimals
-    bool public _airdropExecuted;
+    address public constant _zkpay = 0x25a5674f3D6Afb27e20820063B4fE2786bcB2e6D;
+    uint256 public constant PAYMENT_AMOUNT = 0.001 ether;
     bytes32 public _queryHash;
 
-    constructor(address zkpay, address demoToken) {
+    event QuerySubmitted(bytes32 queryHash);
+    event PaymentCalculated(uint256 totalPayment, uint256 queryAmount);
+    event QueryCancelled(bytes32 queryHash);
+    event WithdrawExecuted(uint256 amount);
+    event FundsReceived(address sender, uint256 amount);
+    event QueryPreFlight(uint256 msgValue, uint256 gasLimit, address caller);
+    event DebugQuery(bytes encodedQuery);
+
+    constructor() {
         _owner = msg.sender;
-        _zkpay = zkpay;
-        _token = IERC20(demoToken);
-        _airdropExecuted = false;
     }
 
     modifier onlyOwner() {
@@ -32,74 +32,55 @@ contract AirdropClient is IZKPayClient {
         _;
     }
 
-    modifier onlyZKPay() {
-        require(msg.sender == _zkpay, "Caller is not _zkpay");
-        _;
-    }
-
-    modifier onlyOnce() {
-        require(!_airdropExecuted, "Airdrop has already been executed");
-        _;
-        _airdropExecuted = true;
-    }
-
     function queryZKPay() external payable onlyOwner {
+        emit QueryPreFlight(msg.value, gasleft(), msg.sender);
+        
         DataTypes.QueryParameter[] memory queryParams;
+        bytes memory query = abi.encode(
+            "SELECT FROM_ADDRESS, COUNT(*) AS TRANSACTION_COUNT FROM ETHEREUM.TRANSACTIONS WHERE TO_ADDRESS = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84' AND FROM_ADDRESS != '0x0000000000000000000000000000000000000000' GROUP BY FROM_ADDRESS ORDER BY TRANSACTION_COUNT DESC LIMIT 10;"
+        );
+        emit DebugQuery(query);
 
         DataTypes.QueryData memory queryData = DataTypes.QueryData({
-            query: abi.encode(
-                "SELECT FROM_ADDRESS, COUNT(*) AS TRANSACTION_COUNT FROM ETHEREUM.TRANSACTIONS WHERE TO_ADDRESS = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84' AND FROM_ADDRESS != '0x0000000000000000000000000000000000000000' GROUP BY FROM_ADDRESS ORDER BY TRANSACTION_COUNT DESC LIMIT 400;"
-            ),
+            query: query,
             queryType: DataTypes.QueryType.SQL,
             queryParameters: queryParams,
             timeout: uint64(block.timestamp + 30 minutes),
             callbackClientContractAddress: address(this),
             callbackGasLimit: 400_000,
-            callbackData: "",
+            callbackData: abi.encode(PAYMENT_AMOUNT),
             zkVerficiation: DataTypes.ZKVerification.External
         });
-
-        _queryHash = IZKPay(_zkpay).queryWithNative{ value: msg.value }(queryData);
-    }
-
-    function sxtCallback(
-        DataTypes.QueryResult calldata queryResult,
-        bytes calldata callbackData,
-        DataTypes.ZKVerification zkVerficiation
-    )
-        external
-        override
-        onlyZKPay
-        onlyOnce
-    {
-        require(_queryHash != 0, "Invalid query hash");
-        require(queryResult.queryHash == _queryHash, "Query hash does not match");
-
-        for (uint256 i = 0; i < queryResult.columns[0].values.length; i++) {
-            address recipient = abi.decode(queryResult.columns[0].values[i], (address));
-            _token.safeTransfer(recipient, AIRDROP_AMOUNT);
+        
+        uint256 totalPayment = PAYMENT_AMOUNT * 10;
+        require(msg.value > totalPayment, "Insufficient ETH sent for payments");
+        
+        uint256 queryAmount = msg.value - totalPayment;
+        emit PaymentCalculated(totalPayment, queryAmount);
+        
+        try IZKPay(_zkpay).queryWithNative{value: queryAmount}(queryData) returns (bytes32 hash) {
+            _queryHash = hash;
+            emit QuerySubmitted(hash);
+        } catch Error(string memory reason) {
+            revert(string(abi.encodePacked("ZKPay query failed: ", reason)));
+        } catch (bytes memory lowLevelData) {
+            revert("ZKPay query failed with low level error");
         }
-    }
-
-    function sxtErrorCallback(
-        DataTypes.ZKpayError calldata error,
-        bytes calldata callbackData
-    )
-        external
-        override
-        onlyZKPay
-    {
-        emit LogError(error.code, error.message);
-    }
-
-    function withdraw() external onlyOwner {
-        (bool success,) = _owner.call{ value: address(this).balance }("");
-        require(success, "Failed to send Ether");
     }
 
     function cancelQuery(bytes32 queryHash) external onlyOwner {
         IZKPay(_zkpay).cancelQueryPayment(queryHash);
+        emit QueryCancelled(queryHash);
     }
 
-    receive() external payable { }
+    function withdraw() external onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success,) = _owner.call{value: amount}("");
+        require(success, "Failed to send Ether");
+        emit WithdrawExecuted(amount);
+    }
+
+    receive() external payable {
+        emit FundsReceived(msg.sender, msg.value);
+    }
 }
